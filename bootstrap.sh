84e634c75ff58eccaddc5e608c797be3cfb00d54
Adrian Rego <arego320@gmail.com>
2011-06-27 01:07:41 -0400

    renaming files

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#git clone git://github.com/adrianrego/dotfiles.git ~/.dotfiles
+
+#rm -f ~/.bash_profile
+#rm -f ~/.profile
+#rm -f ~/.gitconfig
+#rm -f ~/.gitexcludes
+#rm -f ~/.hgrc
+#rm -f -R ~/.vim*
+#rm -f -R ~/.gvim*
+
+for f in $(ls .)
+do
+
+done
+
+
+#ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+#ln -s ~/.dotfiles/profile ~/.profile
+#ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/.dotfiles/gitexcludes ~/.gitexcludes
+#ln -s ~/.dotfiles/hgrc ~/.hgrc
+#ln -s ~/.dotfiles/janus.rake ~/.janus.rake
+#ln -s ~/.dotfiles/vimrc.local ~/
+
+
