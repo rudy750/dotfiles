e50042d255a2074a23fe53eecde6f77d7d24043f
Adrian Rego <arego320@gmail.com>
2011-08-19 00:12:47 -0400

    getting experimental

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ rm -f -R $DOTFILES_HOME
 # Clone your dotfiles repo
 echo " -- Cloning dotfiles -- "
 echo
-git clone $DOTFILES_REPO $DOTFILES_HOME
+git clone -b experimental $DOTFILES_REPO $DOTFILES_HOME
 
 echo " -- Linking files -- "
 for f in $(ls $DOTFILES_HOME)
@@ -32,10 +32,41 @@ echo " -- Bash IT -- "
 # Install bash_it
 git clone git://github.com/revans/bash-it.git ~/.bash_it
 
+BASH_PLUGINS="base.plugin.bash
+extract.plugin.bash
+git.plugins.bash
+rvm.plugin.bash
+vagrant.plugin.bash
+virtualenv.plugin.bash"
+
+BASH_ALIASES="general"
+
+BASH_COMPLETION="git
+git_flow"
+
+mkdir $HOME/.bash_it/plugins/enabled
+mkdir $HOME/.bash_it/aliases/enabled
+mkdir $HOME/.bash_it/completion/enabled
+
+for p in $BASH_PLUGINS
+do
+  ln -s $HOME/.bash_it/plugins/available/$p $HOME/.bash_it/plugins/enabled/$p
+done
+
+for a in $BASH_ALIASES
+do
+  ln -s $HOME/.bash_it/aliases/available/$a.aliases.bash $HOME/.bash_it/aliases/enabled/$a.aliases.bash
+done
+
+for c in $BASH_COMPLETION
+do
+  ln -s $HOME/.bash_it/completion/available/$c.completion.bash $HOME/.bash_it/completion/enabled/$c.completion.bash
+done
+
 echo
 echo " -- Janus -- "
 # Install Janus
-git clone git://github.com/carlhuda/janus.git ~/.vim
+git clone -b experimental git://github.com/adrianrego/janus.git ~/.vim
 cd ~/.vim
 rake
 
--- janus.rake
+++
@@ -1,6 +0,0 @@
-skip_vim_plugin "taglist"
-
-vim_plugin_task "bufexplorer",        "git://github.com/vim-scripts/bufexplorer.zip.git"
-vim_plugin_task "tagbar",             "git://github.com/majutsushi/tagbar.git"
-vim_plugin_task "pep8",               "git://github.com/nvie/vim-pep8.git"
-vim_plugin_task "PIV",                "git://github.com/spf13/PIV.git"
